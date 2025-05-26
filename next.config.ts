3ea822044edfa9b639072c9fe05e2aa6117fd2d7
mohamad-serhan1 <mohd.ser19@gmail.com>
2025-05-26 23:03:22 +0300

    m

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: true, // disable PWA temporarily
   register: true,
   skipWaiting: true,
 });
 
 const nextConfig = {
   reactStrictMode: true,
-
 };
 
 module.exports = withPWA(nextConfig);
